728c9d2508ebffe73d83b9e92f450bca7b119240
Matthew W <fatboychummy@gmail.com>
2023-12-18 04:09:08 -0700

    Refactor test_runner.lua to include logger functionality

--- Framework/test_runner.lua
+++ Framework/test_runner.lua
@@ -10,12 +10,15 @@ local expect = require "cc.expect".expect --[[@as fun(a: number, b: any, ...: st
 
 --- Run a single test.
 ---@param test test_data The test to run.
+---@param logger logger The logger to use.
 ---@return test_data test The test, with updated status. It is not required to grab the return value, as it modifies the table in-place.
 local function run_test(test, logger)
   expect(1, test, "table")
+  logger.new_test(test.name)
 
   -- If the test is disabled, skip it.
   if test.status == "disabled" then
+    logger.update_status "disabled"
     return test
   end
 
@@ -26,6 +29,7 @@ local function run_test(test, logger)
     local resume_immediate = false
 
     test.status = "running"
+    logger.update_status "running"
     while true do
       local event
 
@@ -41,11 +45,18 @@ local function run_test(test, logger)
       -- Resume the test's coroutine.
       ok, event_filter, test_assertion, message = coroutine.resume(coro, table.unpack(event, 1, event.n))
 
-
       if not ok then
         -- If the coroutine errored, the test errored. Report and exit.
         test.status = "error"
         test.error = event_filter
+
+        logger.update_status "error"
+
+        if logger.verbose then
+          logger.log_stacktrace(debug.traceback(coro, event_filter))
+        else
+          logger.log_error(event_filter)
+        end
         break
       elseif coroutine.status(coro) == "dead" then
         -- If the coroutine is dead, the test has ended.
@@ -53,17 +64,24 @@ local function run_test(test, logger)
         if test.status == "running" then
           test.status = "pass"
         end
+
+        logger.update_status(test.status)
         break
       elseif event_filter:match("^cctest:") then
         if event_filter == "cctest:fail_assertion" then
           -- If an assertion failed, the test failed. Report and exit.
           test.status = "fail"
           table.insert(test.failures, message)
+          logger.update_status "fail"
+          logger.log_assertion(test_assertion, false, message)
+
           break
         elseif event_filter == "cctest:fail_expectation" then
           -- If an expectation failed, the test should continue, but is still marked as a failure.
           test.status = "fail"
           table.insert(test.failures, message)
+          logger.update_status "fail"
+          logger.log_expectation(test_assertion, false, message)
 
           -- We need to resume the coroutine immediately.
           resume_immediate = true
@@ -71,6 +89,12 @@ local function run_test(test, logger)
           -- If an assertion or expectation passed, the test should continue.
           -- We need to resume the coroutine immediately.
           resume_immediate = true
+
+          if test_assertion:match("^EXPECT_") then
+            logger.log_expectation(test_assertion, true)
+          else
+            logger.log_assertion(test_assertion, true)
+          end
         end
       end
     end
