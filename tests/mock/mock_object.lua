89b61df23b134134917f3c2981e52efadd8e707d
Matthew W <fatboychummy@gmail.com>
2024-05-28 21:53:37 -0600

    Disable mock object testing until we get it fully implemented.

--- tests/mock/mock_object.lua
+++ tests/mock/mock_object.lua
@@ -5,7 +5,7 @@ local mock_obj = mock.new("John", {})
 ---@diagnostic disable: undefined-global We will be using a bunch of globals here defined in another location.
 
 suite.suite "Mock objects"
-  "NOOT NOOT" (function()
+  "NOOT NOOT" (suite.MODS.DISABLE, function()
     mock_obj.MOCK_METHOD("test")
       .RETURN_ONCE("test")
       .EXPECT_CALL(1)
